27ba14fc628ad5d51c55d76b8e0222673726dbff
CHERUGAD <ravikiranpcherugad@gmail.com>
2025-01-23 21:19:02 +0530

    two loops are used to seperate even and odd numbers in a single list

--- Even_and_odd_seperation.py
+++ Even_and_odd_seperation.py
@@ -9,3 +9,17 @@ for number in element:
         odd_numbers.append(number)
 new_list=even_numbers+odd_numbers
 print(new_list)
+
+###Using while loop
+elementt=[1, 2, 3, 2, 4, 2, 5]
+even_numbers=[]
+odd_numbers=[]
+i=0
+while i<len(elementt):
+    if elementt[i]%2==0:
+        even_numbers.append(elementt[i])
+    else:
+        odd_numbers.append(elementt[i])
+    i+=1
+new_listt=even_numbers+odd_numbers
+print(new_listt)
