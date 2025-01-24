aa6f61cfd206b6420fc916e6594941e8e200de4c
CHERUGAD <ravikiranpcherugad@gmail.com>
2025-01-24 16:21:01 +0530

    Intersection betweeen two list is identified

--- find_intersection.py
+++ find_intersection.py
@@ -4,8 +4,9 @@ list_2=[1,2,3,4,5,6,7,8]
 new_list=[]
 i=0
 while i<len(list_1):
-    for list1 in list_2:
-        if list1==list_1[i]:
-            new_list.append(list1)
-i+=1
+    for element in list_2:
+        if list_1[i]==element:
+            new_list.append(list_1[i])
+            break
+    i+=1
 print(new_list)
