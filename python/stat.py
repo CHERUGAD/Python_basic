31a07642c21184b0c85ddbc1f101906bed7d3e92
CHERUGAD <ravikiranpcherugad@gmail.com>
2025-02-28 14:27:25 +0530

    Using statistcs built in function

---
+++ python/stat.py
@@ -0,0 +1,7 @@
+import statistics
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(statistics.mean(my_list))      # Mean
+print(statistics.median(my_list))    # Median
+print(statistics.mode(my_list))      # Mode
+print(statistics.stdev(my_list))     # Standard Deviation
+print(statistics.variance(my_list))  # Variance
