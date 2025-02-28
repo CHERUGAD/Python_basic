f66356fd08068d325f1681baf1beb9974306ac8b
CHERUGAD <ravikiranpcherugad@gmail.com>
2025-02-28 12:44:17 +0530

    Using classes simple mathematical opereration has been done

---
+++ python.py/classes.py
@@ -0,0 +1,15 @@
+#Using class 
+class Calculator:
+    def add(x,y):
+        return x+y
+    def sub(x,y):
+        return x-y
+    def mul(x,y):
+        return x*y
+    def div(x,y):
+        return x/y
+#Creating object of class
+print(Calculator.add(10,20))
+print(Calculator.sub(20,10))
+print(Calculator.mul(10,20))
+print(Calculator.div(20,10))
